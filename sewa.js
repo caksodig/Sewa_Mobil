c7a3332c50f6e1d1270bbc48200e0a5430596aaa
caksodig <didig1603@gmail.com>
2023-02-02 13:45:21 +0700

    first commit

---
+++ sewa.js
@@ -0,0 +1,460 @@
+// insialisasi library
+const express = require("express")
+const app = express()
+const multer = require("multer") // untuk upload file
+const path = require("path") // untuk memanggil path direktori
+const fs = require("fs") // untuk manajemen file
+const mysql = require("mysql")
+const cors = require("cors")
+
+// implementasi
+app.use(express.static(__dirname));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+// variabel untuk konfigurasi proses upload file
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        // set file storage
+        cb(null, './image');
+    },
+    filename: (req, file, cb) => {
+        // generate file name 
+        cb(null, "image-" + Date.now() + path.extname(file.originalname))
+    }
+})
+
+let upload = multer({ storage: storage })
+
+// mysql connection
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "rent_car"
+})
+
+// endpoint untuk menambah data penyewaan mobil
+
+app.post("/mobil", upload.single("image"), (req, res) => {
+    // prepare data
+    let data = {
+        nomor_mobil: req.body.nomor_mobil,
+        merk: req.body.merk,
+        jenis: req.body.jenis,
+        warna: req.body.warna,
+        tahun_pembuatan: req.body.tahun_pembuatan,
+        biaya_sewa_per_hari: req.body.biaya_sewa_per_hari,
+        image: req.file.filename
+    }
+
+    if (!req.file) {
+        // jika tidak ada file yang diupload
+        res.json({
+            message: "Tidak ada file yang dikirim"
+        })
+    } else {
+        // create sql insert
+        let sql = "insert into mobil set ?"
+
+        // run query
+        db.query(sql, data, (error, result) => {
+            if (error) throw error
+            res.json({
+                message: result.affectedRows + " data berhasil disimpan"
+            })
+        })
+    }
+})
+
+// endpoint untuk mengubah data penyewaan mobil
+app.put("/mobil", upload.single("image"), (req, res) => {
+    let data = null, sql = null
+    // paramter perubahan data
+    let param = { id_mobil: req.body.id_mobil }
+
+    if (!req.file) {
+        // jika tidak ada file yang dikirim = update data saja
+        data = {
+            nomor_mobil: req.body.nomor_mobil,
+            merk: req.body.merk,
+            jenis: req.body.jenis,
+            warna: req.body.warna,
+            tahun_pembuatan: req.body.tahun_pembuatan,
+            biaya_sewa_per_hari: req.body.biaya_sewa_per_hari,
+        }
+    } else {
+        // jika mengirim file = update data + reupload
+        data = {
+            nomor_mobil: req.body.nomor_mobil,
+            merk: req.body.merk,
+            jenis: req.body.jenis,
+            warna: req.body.warna,
+            tahun_pembuatan: req.body.tahun_pembuatan,
+            biaya_sewa_per_hari: req.body.biaya_sewa_per_hari,
+            image: req.file.filename
+        }
+
+        // get data yg akan diupdate utk mendapatkan nama file yang lama
+        sql = "select * from mobil where ?"
+        // run query
+        db.query(sql, param, (err, result) => {
+            if (err) throw err
+            // tampung nama file yang lama
+            let fileName = result[0].image
+
+            // hapus file yg lama
+            let dir = path.join(__dirname, "image", fileName)
+            fs.unlink(dir, (error) => { })
+        })
+
+    }
+
+    // create sql update
+    sql = "update mobil set ? where ?"
+
+    // run sql update
+    db.query(sql, [data, param], (error, result) => {
+        if (error) {
+            res.json({
+                message: error.message
+            })
+        } else {
+            res.json({
+                message: result.affectedRows + " data berhasil diubah"
+            })
+        }
+    })
+})
+
+// endpoint ambil data penyewaan mobil
+app.get("/mobil", (req, res) => {
+    // create sql query
+    let sql = "select * from mobil"
+
+    // run query
+    db.query(sql, (error, result) => {
+        if (error) throw error
+        res.json({
+            data: result,
+            count: result.length
+        })
+    })
+})
+
+// endpoint untuk menghapus data penyewaan mobil
+app.delete("/mobil/:id_mobil", (req, res) => {
+    let param = { id_mobil: req.params.id_mobil }
+
+    // ambil data yang akan dihapus
+    let sql = "select * from mobil where ?"
+    // run query
+    db.query(sql, param, (error, result) => {
+        if (error) throw error
+
+        // tampung nama file yang lama
+        let fileName = result[0].image
+
+        // hapus file yg lama
+        let dir = path.join(__dirname, "image", fileName)
+        fs.unlink(dir, (error) => { })
+    })
+
+    // create sql delete
+    sql = "delete from mobil where ?"
+
+    // run query
+    db.query(sql, param, (error, result) => {
+        if (error) {
+            res.json({
+                message: error.message
+            })
+        } else {
+            res.json({
+                message: result.affectedRows + " data berhasil dihapus"
+            })
+        }
+    })
+})
+
+// end-point menyimpan data pelanggan
+app.post("/pelanggan", (req, res) => {
+
+    // prepare data
+    let data = {
+        nama_pelanggan: req.body.nama_pelanggan,
+        alamat_pelanggan: req.body.alamat_pelanggan,
+        kontak: req.body.kontak
+    }
+
+    // create sql query insert
+    let sql = "insert into pelanggan set ?"
+
+    // run query
+    db.query(sql, data, (error, result) => {
+        let response = null
+        if (error) {
+            response = {
+                message: error.message
+            }
+        } else {
+            response = {
+                message: result.affectedRows + " data inserted"
+            }
+        }
+        res.json(response) // send response
+    })
+})
+
+// end-point mengubah data pelanggan
+app.put("/pelanggan", (req, res) => {
+
+    // prepare data
+    let data = [
+        // data
+        {
+            nama_pelanggan: req.body.nama_pelanggan,
+            alamat_pelanggan: req.body.alamat_pelanggan,
+            kontak: req.body.kontak
+        },
+
+        // parameter (primary key)
+        {
+            id_pelanggan: req.body.id_pelanggan
+        }
+    ]
+
+    // create sql query update
+    let sql = "update pelanggan set ? where ?"
+
+    // run query
+    db.query(sql, data, (error, result) => {
+        let response = null
+        if (error) {
+            response = {
+                message: error.message
+            }
+        } else {
+            response = {
+                message: result.affectedRows + " data updated"
+            }
+        }
+        res.json(response) // send response
+    })
+})
+
+// end-point akses data pelanggan
+app.get("/pelanggan", (req, res) => {
+    // create sql query
+    let sql = "select * from pelanggan"
+
+    // run query
+    db.query(sql, (error, result) => {
+        let response = null
+        if (error) {
+            response = {
+                message: error.message // pesan error
+            }
+        } else {
+            response = {
+                count: result.length, // jumlah data
+                pelanggan: result // isi data
+            }
+        }
+        res.json(response) // send response
+    })
+})
+
+
+// end-point menghapus data pelanggan berdasarkan id_pelanggan
+app.delete("/pelanggan/:id", (req, res) => {
+    // prepare data
+    let data = {
+        id_pelanggan: req.params.id
+    }
+
+    // create query sql delete
+    let sql = "delete from pelanggan where ?"
+
+    // run query
+    db.query(sql, data, (error, result) => {
+        let response = null
+        if (error) {
+            response = {
+                message: error.message
+            }
+        } else {
+            response = {
+                message: result.affectedRows + " data deleted"
+            }
+        }
+        res.json(response) // send response
+    })
+})
+
+// end-point menyimpan data karyawan
+app.post("/karyawan", (req, res) => {
+
+    // prepare data
+    let data = {
+        nama_karyawan: req.body.nama_karyawan,
+        alamat_karyawan: req.body.alamat_karyawan,
+        kontak: req.body.kontak,
+        username: req.body.username,
+        password: req.body.password
+    }
+
+    // create sql query insert
+    let sql = "insert into karyawan set ?"
+
+    // run query
+    db.query(sql, data, (error, result) => {
+        let response = null
+        if (error) {
+            response = {
+                message: error.message
+            }
+        } else {
+            response = {
+                message: result.affectedRows + " data inserted"
+            }
+        }
+        res.json(response) // send response
+    })
+})
+
+// end-point mengubah data karyawan
+app.put("/karyawan", (req, res) => {
+
+    // prepare data
+    let data = [
+        // data
+        {
+            nama_karyawan: req.body.nama_karyawan,
+            alamat_karyawan: req.body.alamat_karyawan,
+            kontak: req.body.kontak,
+            username: req.body.username,
+            password: req.body.password
+        },
+
+        // parameter (primary key)
+        {
+            id_karyawan: req.body.id_karyawan
+        }
+    ]
+
+    // create sql query update
+    let sql = "update karyawan set ? where ?"
+
+    // run query
+    db.query(sql, data, (error, result) => {
+        let response = null
+        if (error) {
+            response = {
+                message: error.message
+            }
+        } else {
+            response = {
+                message: result.affectedRows + " data updated"
+            }
+        }
+        res.json(response) // send response
+    })
+})
+
+// end-point akses data karyawan
+app.get("/karyawan", (req, res) => {
+    // create sql query
+    let sql = "select * from karyawan"
+
+    // run query
+    db.query(sql, (error, result) => {
+        let response = null
+        if (error) {
+            response = {
+                message: error.message // pesan error
+            }
+        } else {
+            response = {
+                count: result.length, // jumlah data
+                karyawan: result // isi data
+            }
+        }
+        res.json(response) // send response
+    })
+})
+
+// end-point menghapus data karyawan berdasarkan id_karyawan
+app.delete("/karyawan/:id", (req, res) => {
+    // prepare data
+    let data = {
+        id_karyawan: req.params.id
+    }
+
+    // create query sql delete
+    let sql = "delete from karyawan where ?"
+
+    // run query
+    db.query(sql, data, (error, result) => {
+        let response = null
+        if (error) {
+            response = {
+                message: error.message
+            }
+        } else {
+            response = {
+                message: result.affectedRows + " data deleted"
+            }
+        }
+        res.json(response) // send response
+    })
+})
+
+// transaksi
+
+// end-point menambahkan data pelanggaran 
+
+// app.post("/sewa", (req, res) => {
+//     // prepare data to sewa
+//     let data = {
+//         id_karyawan: req.body.id_karyawan,
+//         id_pelanggan: req.body.id_pelanggan,
+//         id_mobil: req.body.id_mobil,
+//         tgl_sewa: req.body.tgl_sewa,
+//         tgl_kembali: req.body.tgl_kembali
+//     }
+
+//     // parse to JSON
+//     let sewa = JSON.parse(req.body.sewa)
+
+//     // create query insert to pelanggaran_siswa
+//     let sql = "insert into sewa set ?"
+
+//     // run query
+//     db.query(sql, data, (error, result) => {
+//         let response = null
+
+//         if (error) {
+//             res.json({ message: error.message })
+//         } else {
+
+//             // get last inserted id_pelanggaran
+//             let lastID = result.insertId
+
+//             // prepare data to detail_pelanggaran
+//             let data = []
+//             for (let index = 0; index < sewa.length; index++) {
+//                 data.push([
+//                     lastID, sewa[index].id_sewa
+//                 ])
+//             }
+//         }
+//     })
+// })
+
+app.listen(3000, () => {
+    console.log("be awesome")
+})
